04b741f6ceeba3c9f9a432e46371038afd82ae65
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2023-08-30 15:40:32 +0200

    Added email, phonenumber and input for two users

--- Frontend/src/SubmitForm/SubmitForm.js
+++ Frontend/src/SubmitForm/SubmitForm.js
@@ -5,6 +5,12 @@ import "./SubmitForm.css";
 const SubmitForm = ({ onSubmitted }) => {
   const [username, setUsername] = useState("");
   const [time, setTime] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [username2, setUsername2] = useState("");
+  const [time2, setTime2] = useState("");
+  const [email2, setEmail2] = useState("");
+  const [phoneNumber2, setPhoneNumber2] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [successfulPost, setSuccessfulPost] = useState(false);
@@ -14,7 +20,7 @@ const SubmitForm = ({ onSubmitted }) => {
     setSuccessfulPost(false);
     setErrorMessage("");
     setLoading(true);
-    fetch(`https://localhost:5050/NewTimeEntry?username=${username}&time=${time}`, {
+    fetch(`https://localhost:5050/NewTimeEntry?username=${username}&email=${email}&phoneNumber=${phoneNumber}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,6 +35,8 @@ const SubmitForm = ({ onSubmitted }) => {
         setErrorMessage("");
         setUsername("");
         setTime("");
+        setEmail("");
+        setPhoneNumber("");
         setSuccessfulPost(true);
       })
       .catch((error) => {
@@ -51,6 +59,31 @@ const SubmitForm = ({ onSubmitted }) => {
     setTime(e.target.value);
   };
 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  }
+
+  const handleUserNameChange2 = (e) => {
+    setSuccessfulPost(false);
+    setUsername2(e.target.value);
+  };
+
+  const handleTimeChange2 = (e) => {
+    setTime2(e.target.value);
+  };
+
+  const handleEmailChange2 = (e) => {
+    setEmail2(e.target.value);
+  }
+
+  const handlePhoneNumberChange2 = (e) => {
+    setPhoneNumber2(e.target.value);
+  }
+
   return (
     <article className="submit-form__wrapper">
       {loading ? (
@@ -58,18 +91,44 @@ const SubmitForm = ({ onSubmitted }) => {
       ) : (
         <form className="submit-form" onSubmit={handleSubmit} id="submit-form" aria-describedby="submit-form__message">
           <fieldset className="submit-form__fieldset">
-            <label className="submit-form__label">Name</label>
-            <input
-              className="submit-form__input"
-              type="text"
-              value={username}
-              onChange={handleUserNameChange}
-              required
-            />
+            <ul>
+              <li>
+                <label className="submit-form__label">Name</label>
+                <input className="submit-form__input" type="text" value={username} onChange={handleUserNameChange} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Time</label>
+                <input className="submit-form__input" type="text" value={time} onChange={handleTimeChange} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Email</label>
+                <input className="submit-form__input" type="text" value={email} onChange={handleEmailChange} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Phone number</label>
+                <input className="submit-form__input" type="text" value={phoneNumber} onChange={handlePhoneNumberChange} required />
+              </li>
+            </ul>
           </fieldset>
           <fieldset className="submit-form__fieldset">
-            <label className="submit-form__label">Time</label>
-            <input className="submit-form__input" type="text" value={time} onChange={handleTimeChange} required />
+            <ul>
+              <li>
+                <label className="submit-form__label">Name</label>
+                <input className="submit-form__input" type="text" value={username2} onChange={handleUserNameChange2} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Time</label>
+                <input className="submit-form__input" type="text" value={time2} onChange={handleTimeChange2} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Email</label>
+                <input className="submit-form__input" type="text" value={email2} onChange={handleEmailChange2} required />
+              </li>
+              <li>
+                <label className="submit-form__label">Phone number</label>
+                <input className="submit-form__input" type="text" value={phoneNumber2} onChange={handlePhoneNumberChange2} required />
+              </li>
+            </ul>
           </fieldset>
           {errorMessage && (
             <p className="submit-form__error" id="submit-form__message">
