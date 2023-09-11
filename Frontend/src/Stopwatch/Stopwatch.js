cbce11c70a81ffb857a13e08444b093ceb20b1d5
cornelia771 <corneliavassbotn@gmail.com>
2023-09-11 09:31:07 +0200

    added reset button

--- Frontend/src/Stopwatch/Stopwatch.js
+++ Frontend/src/Stopwatch/Stopwatch.js
@@ -227,6 +227,7 @@ function Stopwatch() {
     <div className="stopwatch">
       <p>{formatTime(elapsedTime)}</p>
       <button className="button" disabled={waitingForStartSignal} onClick={reset} >{waitingForStartSignal ? "Waiting for start signal" : "New round"}</button>
+      <button className="button" onClick={reset}>Reset</button>
       {lapTime !== 0 && (
         <p>{lapTime}</p>
       )}
