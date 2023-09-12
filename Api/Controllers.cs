2270140d4a4d0d31438398868df80ea775560074
Burhan Mohammad Sarfraz <burhan.sarfraz@bouvet.no>
2023-09-12 09:06:43 +0200

    Hide "waiting for start signal" button during run

--- Api/Controllers.cs
+++ Api/Controllers.cs
@@ -4,54 +4,55 @@ public static class Controllers
   {
     app.MapGet("/GetCurrentTopListName", string (Application application) =>
     {
-      return application.CurrentTopListFileName;
+        return application.CurrentTopListFileName;
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/GetCurrentTopList", IResult (Application application) =>
     {
-      return Results.Ok(application.CurrentToplist.OrderBy(x => x.Time).ToList());
+        return Results.Ok(application.CurrentToplist.OrderBy(x => x.Time).ToList());
     }).Produces<List<User>>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/GetAllUsersList", IResult (Application application) =>
     {
-      return Results.Ok(application.CurrentToplist.ToList());
-    }).Produces<List<User>>(StatusCodes.Status200OK).Produces<string>(StatusCodes.Status400BadRequest);
+        return Results.Ok(application.CurrentToplist.ToList());
+    }).Produces<List<User>>(StatusCodes.Status200OK)
+      .Produces<string>(StatusCodes.Status400BadRequest);
 
-    // app.MapGet("/GetNewUsers", IResult (Application application) =>
-    // {
-    //   return Results.Ok(application.CurrentToplistFileName.ToList());
-    // }).Produces<List<User>>(StatusCodes.Status200OK)
-    //   .Produces<string>(StatusCodes.Status400BadRequest);
+    //app.MapGet("/GetNewUsers", IResult (Application application) =>
+    //{
+    //    return Results.Ok(application.CurrentToplistFileName.ToList());
+    //}).Produces<List<User>>(StatusCodes.Status200OK)
+    //  .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/ListAllTopLists", IResult (Application application) =>
     {
-      return application.ListAllTopLists();
+        return application.ListAllTopLists();
     }).Produces<List<string>>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapPost("/CreateNewTopList", (string topListName, Application application) =>
     {
-      return application.CreateNewTopList(topListName);
+        return application.CreateNewTopList(topListName);
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapPost("/LoadTopList", async (string topListName, Application application) =>
     {
-      return await application.LoadTopListAsync(topListName);
+        return await application.LoadTopListAsync(topListName);
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapPost("/NewTimeEntry", async (string username, string email, int phoneNumber, string username2, string email2, int phoneNumber2, Application application) =>
     {
-      return await application.NewTimeEntry(username, email, phoneNumber, username2, email2, phoneNumber2);
+        return await application.NewTimeEntry(username, email, phoneNumber, username2, email2, phoneNumber2);
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/StartTime", bool (Application application) =>
     {
-      return application.StartTime();
+        return application.StartTime();
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
@@ -65,19 +66,7 @@ public static class Controllers
     {
         return application.EndTime2();
     }).Produces<string>(StatusCodes.Status200OK)
-        .Produces<string>(StatusCodes.Status400BadRequest);
-
-    // app.MapGet("/simulateEndTime", void (Application application) =>
-    // {
-    //  application.setStopTime();
-    // }).Produces<string>(StatusCodes.Status200OK)
-    // .Produces<string>(StatusCodes.Status400BadRequest);
-
-    // app.MapGet("/simulateEndTime2", void (Application application) =>
-    // {
-    //  application.setStopTime2();
-    // }).Produces<string>(StatusCodes.Status200OK)
-    //  .Produces<string>(StatusCodes.Status400BadRequest);
+      .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/simulateStartTime", void (Application application) =>
     {
@@ -85,21 +74,33 @@ public static class Controllers
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
+    //app.MapGet("/simulateEndTime", void (Application application) =>
+    //{
+    //    application.setStopTime();
+    //}).Produces<string>(StatusCodes.Status200OK)
+    //  .Produces<string>(StatusCodes.Status400BadRequest);
+
+    //app.MapGet("/simulateEndTime2", void (Application application) =>
+    //{
+    //    application.setStopTime2();
+    //}).Produces<string>(StatusCodes.Status200OK)
+    //  .Produces<string>(StatusCodes.Status400BadRequest);
+
     app.MapGet("/sendTimePlayer1", string (Application application) =>
     {
-      return application.sendTimePlayer1();
+        return application.sendTimePlayer1();
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/sendTimePlayer2", string (Application application) =>
     {
-      return application.sendTimePlayer2();
+        return application.sendTimePlayer2();
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
     app.MapGet("/ResetTime", void (Application application) =>
     {
-      application.resetTime();
+        application.resetTime();
     }).Produces<string>(StatusCodes.Status200OK)
       .Produces<string>(StatusCodes.Status400BadRequest);
 
